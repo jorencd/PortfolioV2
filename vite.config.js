97ee7cf951ae7c2c1c5980caea4b0d7b56eb19aa
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-10-26 20:36:10 +0800

    feat: fix email-error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+  build: {
+    rollupOptions: {
+      external: ['emailjs-com'],  // Add this line
+    },
+  },
+});
