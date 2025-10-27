35e282c604ab3b5af66647d0e1b50cc57de709d8
Jorence Mendoza <jorencemendoza2@gmail.com>
2025-10-27 10:27:43 +0800

    feat: fix emailjs

--- src/modules/section/Contact.jsx
+++ src/modules/section/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "https://esm.sh/emailjs-com";
 
 function Contact() {
   const [formData, setFormData] = useState({
